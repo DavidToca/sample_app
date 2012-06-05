ff506f286b750f79199876863c53d80cfc9995d6
vagrant <vagrant@lucid32.(none)>
2012-06-06 00:54:06 +0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '48f1e4ecf3af807d56394b7a6dd188c0ae8e3c8b6773f1352d650bdfd818a9345ac849d1e00de966eb9708f959fbe61db97de41d71cb3b926083cc44a8dd5159'

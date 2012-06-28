bf7bb9a3730d6da023e5d2e6c252b244f6547552
David Toca <david.virusfel@gmail.com>
2012-06-28 02:29:41 -0500

    @WIP chapter 6.3.4

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,9 +11,11 @@
 
 class User < ActiveRecord::Base
 
-  attr_accessor :password, :password_confirmation
- #:password, :password_confirmation  :something
-  attr_accessible :name, :email, :password, :password_confirmation
+  attr_accessor :password, :password_confirmation, :password_digest
+
+  attr_accessible :name, :email, :password, :password_confirmation, :password_digest
+
+  has_secure_password
 
   before_save {|user| user.email = email.downcase }
 
@@ -21,6 +23,10 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
   uniqueness: { case_sensitive: false }
+
+  validates :password, presence: true, length: { minimum: 6 }
+
+  validates :password_confirmation, presence: true
 end
 
 

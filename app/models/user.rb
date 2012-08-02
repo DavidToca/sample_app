9439a82da0d9e848f0a8f2d24e4b883a344b60e2
David Toca <david.virusfel@gmail.com>
2012-08-01 22:29:49 -0500

    just skipping things

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,22 +11,35 @@
 
 class User < ActiveRecord::Base
 
+#include BCrypt
+
   attr_accessor :password, :password_confirmation, :password_digest
 
   attr_accessible :name, :email, :password, :password_confirmation, :password_digest
 
-  has_secure_password
+#  has_secure_password
 
   before_save {|user| user.email = email.downcase }
 
   validates :name, presence: true , length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
-  uniqueness: { case_sensitive: false }
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+#  uniqueness: { case_sensitive: false }
 
   validates :password, presence: true, length: { minimum: 6 }
 
-  validates :password_confirmation, presence: true
+#  def password
+#    @passsword ||= Password.new(password_digess)
+#  end
+
+#  def password=(new_password)
+#    @password = Password.create(new_password)
+#    self.password_digest=@password
+#  end
+#  validates :password_confirmation, presence: true
+#*/
+  
+
 end
 
 
